ceea3b81a7642b39d172978350b0271705193ec5
anjaligh <anjali93ravindran@gmail.com>
2022-06-14 14:52:14 +0530

    insertion sorting

--- sort.js
+++ sort.js
@@ -10,23 +10,42 @@
 //     console.log("num2= "+num2)
 // }
 
+// // // BUBBLE SORTING
+
+// arrayIn = [16, 26, 3, 14, 6,87];
+// len = arrayIn.length;
+// console.log(len)
+// for (j = 0; j < len; j++) {
+//     for (i = 0; i < len - 1; i++) {
+//         // console.log("len=" + len)
+//         // console.log("i=" + i);
+//         // console.log("input" + arrayIn)
+//         if (arrayIn[i] > arrayIn[i + 1]) {
+//             num = arrayIn[i];
+//             arrayIn[i] = arrayIn[i + 1];
+//             arrayIn[i + 1] = num;
+//             // console.log(arrayIn)
+//         }
+//         console.log(arrayIn)
+//         arrayIn = arrayIn;
+//     }
+// }
+
+// //INSERTION SORT
 
 arrayIn = [16, 26, 3, 14, 6,87];
 len = arrayIn.length;
-console.log(len)
-for (j = 0; j < len; j++) {
-    for (i = 0; i < len - 1; i++) {
-        // console.log("len=" + len)
-        // console.log("i=" + i);
-        // console.log("input" + arrayIn)
-        if (arrayIn[i] > arrayIn[i + 1]) {
-            num = arrayIn[i];
-            arrayIn[i] = arrayIn[i + 1];
-            arrayIn[i + 1] = num;
-            // console.log(arrayIn)
-        }
-        console.log(arrayIn)
-        arrayIn = arrayIn;
+// console.log(len)
+for(i=1;i<len;i++){
+    for (j=i;j>0;j--){
+        // console.log(arrayIn[j-1]);
+     if(arrayIn[j]< arrayIn[j-1]){
+         num=arrayIn[j]
+         arrayIn[j]=arrayIn[j-1]
+         arrayIn[j-1]=num;
+        //  console.log(arrayIn)
+     }   
+        
     }
+    console.log(arrayIn)
 }
-
